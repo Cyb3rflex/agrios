98abe63a3f7a11167a0a4ea03c3007bf245667a1
Abdulmuheez <abdulmuheezq@gmail.com>
2024-11-14 15:34:10 +0100

    Initial project setup

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+// client/src/pages/Dashboard.jsx
+// import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold">Admin Dashboard</h1>
+      {/* Add your dashboard components here */}
+    </div>
+  );
+};
+
+export default Dashboard;
